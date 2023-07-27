ab6f3e14f0a0303abb83472ab360c8e59564c6cf
zxx02 <xiaoxunz888@qq.com>
2023-07-27 16:13:56 +0800

    first commit

---
+++ src/pcd_merge.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <string>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+
+#include <ros/package.h>
+#include <ros/ros.h>
+
+#include <pcl/common/common_headers.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/console/parse.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "pcd_merge_node");
+    ros::NodeHandle nh("~");
+
+    double min_x_, max_x_, min_y_, max_y_, min_z_, max_z_;
+    double filter_leaf_size_;
+    std::string merge_pcd_file_name_;
+
+    if (!nh.param("min_x", min_x_, -50.0))
+    {
+        ROS_WARN("No param min_x");
+    }
+    nh.param("max_x", max_x_, 50.0);
+    nh.param("min_y", min_y_, -50.0);
+    nh.param("max_y", max_y_, 50.0);
+    nh.param("min_z", min_z_, 0.0);
+    nh.param("max_z", max_z_, 50.0);
+    nh.param("filter_leaf_size", filter_leaf_size_, 0.1);
+    nh.param("merged_pcd_name", merge_pcd_file_name_, std::string("merged.pcd"));
+
+    // pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    // pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_add(new pcl::PointCloud<pcl::PointXYZI>);
+    // pcl::PointCloud<pcl::PointXYZI>::Ptr filteredCloud(new pcl::PointCloud<pcl::PointXYZI>());
+    // pcl::VoxelGrid<pcl::PointXYZI> filter;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_add(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filteredCloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::VoxelGrid<pcl::PointXYZ> filter;
+
+    // Create the filtering object
+    pcl::PassThrough<pcl::PointXYZ> pass;
+
+    DIR *pDir;
+    struct dirent *ptr;
+
+    std::string package_name = "pcd_manage";
+    std::string package_path = ros::package::getPath(package_name);
+    std::string pcd_files_path = package_path + string("/pcd_raw/");
+    if (!(pDir = opendir(pcd_files_path.c_str())))
+    {
+        std::cout << "Folder doesn't Exist!" << std::endl;
+        return -1;
+    }
+
+    std::string pcd_save_addr = package_path + string("/pcd_merged/");
+    std::string merge_pcd_file_absaddr_ = pcd_save_addr + merge_pcd_file_name_;
+
+    while ((ptr = readdir(pDir)) != 0)
+    {
+        if (strcmp(ptr->d_name, ".") != 0 && strcmp(ptr->d_name, "..") != 0)
+        {
+            pcl::io::loadPCDFile(pcd_files_path + string(ptr->d_name), *cloud);
+
+            pass.setInputCloud(cloud);
+            pass.setFilterFieldName("x");
+            pass.setFilterLimits(min_x_, max_x_);
+            pass.filter(*filteredCloud);
+
+            pass.setInputCloud(filteredCloud);
+            pass.setFilterFieldName("y");
+            pass.setFilterLimits(min_y_, max_y_);
+            pass.filter(*filteredCloud);
+
+            pass.setFilterFieldName("z");
+            pass.setFilterLimits(min_z_, max_z_);
+            pass.filter(*filteredCloud);
+
+            if (filter_leaf_size_ > 0)
+            {
+                filter.setInputCloud(filteredCloud);
+                filter.setLeafSize(filter_leaf_size_, filter_leaf_size_, filter_leaf_size_);
+                filter.filter(*filteredCloud);
+            }
+
+            *cloud_add = *cloud_add + *filteredCloud;
+            cloud->clear();
+            filteredCloud->clear();
+            std::cout << pcd_files_path + ptr->d_name << endl;
+        }
+    }
+    closedir(pDir);
+
+    pcl::io::savePCDFileBinary(merge_pcd_file_absaddr_, *cloud_add);
+
+    ROS_WARN("finish pcd merge!");
+    return 0;
+}
---
+++ src/pcd_rviz.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+#include <string>
+
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/voxel_grid.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "pcl_rviz_node");
+    ros::NodeHandle nh;
+
+    string pcd_file = argv[1];
+
+    sensor_msgs::PointCloud2 cloud_msg;
+    ros::Publisher cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/full_cloud", 10);
+
+    // pcl::PointCloud<pcl::PointXYZINormal>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZINormal>());
+    // pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
+
+    if (pcl::io::loadPCDFile(pcd_file, *cloud) == -1)
+    {
+        PCL_ERROR("Couldn't read file cave_full_pcd.pcd ^.^\n");
+        return (-1);
+    }
+
+    pcl::toROSMsg(*cloud, cloud_msg);
+    ros::Duration(2).sleep();
+    while (ros::ok())
+    {
+        cloud_msg.header.stamp = ros::Time::now();
+        cloud_msg.header.frame_id = "map";
+        cloud_pub.publish(cloud_msg);
+
+        ros::Duration(10).sleep();
+
+        ros::spinOnce();
+    }
+
+    return 0;
+}
